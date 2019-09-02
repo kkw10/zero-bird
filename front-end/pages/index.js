308cf72ab1f1efe3a27bba270e82d7c1c818e498
kkw_10 <kkw_10@naver.com>
2019-09-02 18:25:46 +0900

    기능: 메뉴에 antd 적용

--- front-end/pages/index.js
+++ front-end/pages/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import Link from 'next/link';
+import Head from 'next/head';
+import AppLayout from '../components/AppLayout';
 
 const Home = () => {
     return (
         <>
-            <Link href="/about"><a>about</a></Link>
-            <div>Hello, Next!</div>        
+            <Head>
+                <title>Zero bird</title>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.22.2/antd.min.css"/>
+            </Head>
+            <AppLayout>
+                <Link href="/about"><a>about</a></Link>
+                <div>Hello, Next!</div>                                    
+            </AppLayout>        
         </>
     )
 } 
