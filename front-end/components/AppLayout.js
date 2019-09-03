8c29ebac07949f11e57f939d7ffd370601845e08
kkw_10 <kkw_10@naver.com>
2019-09-03 23:12:25 +0900

    기능: 레이아웃 좌측 로그인폼 컴포넌트로 분리 및 커스텀훅 적용

--- front-end/components/AppLayout.js
+++ front-end/components/AppLayout.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
-import { Menu, Input, Button, Row, Col, Card, Avatar } from 'antd';
+import { Menu, Input, Button, Row, Col, Card, Avatar, Form } from 'antd';
+import LoginForm from '../components/LoginForm';
 
 const dummy = {
     nickname: 'Koon',
     Post: [],
     Followings: [],
-    Followers: [] 
+    Followers: [],
+    isLoggedIn: false
 }
 
 const AppLayout = ({ children }) => {
@@ -24,23 +26,23 @@ const AppLayout = ({ children }) => {
                     <Input.Search enterButton style={{ verticalAlign: 'middle' }} />
                 </Menu.Item>
             </Menu>
-            <Link href="/signup">
-                <a><Button>회원가입</Button></a>
-            </Link>
             <Row>
                 <Col xs={24} md={6}>
-                    <Card
-                        actions={[
-                            <div key="twit">짹짹<br/>{dummy.Post.length}</div>,
-                            <div key="following">팔로잉<br/>{dummy.Followings.length}</div>,
-                            <div key="follower">팔로워<br/>{dummy.Followers.length}</div>
-                        ]}
-                    >
-                        <Card.Meta
-                            avatar={<Avatar>{dummy.nickname[0]}</Avatar>}
-                            title={dummy.nickname}
-                        />
-                    </Card>
+                    {dummy.isLoggedIn 
+                        ?<Card
+                            actions={[
+                                <div key="twit">짹짹<br/>{dummy.Post.length}</div>,
+                                <div key="following">팔로잉<br/>{dummy.Followings.length}</div>,
+                                <div key="follower">팔로워<br/>{dummy.Followers.length}</div>
+                            ]}
+                        >
+                            <Card.Meta
+                                avatar={<Avatar>{dummy.nickname[0]}</Avatar>}
+                                title={dummy.nickname}
+                            />
+                        </Card>
+                        :<LoginForm />
+                    }
                 </Col>
                 <Col xs={24} md={12}>
                     {children}
@@ -49,7 +51,6 @@ const AppLayout = ({ children }) => {
 
                 </Col>
             </Row>
-            
         </div>
     )
 }
