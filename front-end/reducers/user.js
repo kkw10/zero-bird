ba1c5ea43258e282ec715de552bb2454d6dee980
kkw_10 <kkw_10@naver.com>
2019-09-07 23:07:36 +0900

    리펙토링: 비동기 action에서 일관된 형식의 naming 적용

--- front-end/reducers/user.js
+++ front-end/reducers/user.js
@@ -1,3 +1,4 @@
+// dummy
 const dummyUser = {     
     nickname: 'Koon',
     post: [],
@@ -12,27 +13,35 @@ export const initialState = {
     user: null
 }
 
-// Action name
-export const LOG_IN = 'LOG_IN';
-export const LOG_OUT = 'LOG_OUT';
-export const SIGN_UP = 'SIGN_UP';
-export const SIGN_UP_SUCCESS = 'SIGN_UP_SUCCESS';
+// Action name (Request, Success, Failure => RSF시리즈는 "비동기 요청"의 경우 사용함 : 즉 리덕스 사가가 필요함)
+export const LOG_IN_REQUEST = 'LOG_IN_REQUEST';
 export const LOG_IN_SUCCESS = 'LOG_IN_SUCCESS';
 export const LOG_IN_FAILURE = 'LOG_IN_FAILURE';
 
+export const LOG_OUT_REQUEST = 'LOG_OUT_REQUEST';
+export const LOG_OUT_SUCCESS = 'LOG_OUT_SUCCESS';
+export const LOG_OUT_FAULURE = 'LOG_OUT_FAILURE';
+
+export const SIGN_UP_REQUEST = 'SIGN_UP_REQUEST';
+export const SIGN_UP_SUCCESS = 'SIGN_UP_SUCCESS';
+export const SIGN_UP_FAILURE = 'SGIN_UP_FAILURE';
+
+// (RFS시리즈가 아닌 경우에는 "동기 요청"이므로 리덕스만 사용해도 된다.)
+
+
 // Action
 export const loginAction = {
-    type: LOG_IN,
+    type: LOG_IN_REQUEST,
     data: {
         nickname: 'Koon'
     }
 }
 export const logoutAction = {
-    type: LOG_OUT
+    type: LOG_OUT_REQUEST
 }
 export const signUpAction = (data) => {
     return {
-        type: SIGN_UP,
+        type: SIGN_UP_REQUEST,
         data       
     }
 }
@@ -40,15 +49,24 @@ export const signUpAction = (data) => {
 // Reducer
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case LOG_IN: {
+        case LOG_IN_REQUEST: {
             return {
                 ...state,
-                isLoggedIn: true,
-                user: dummyUser
+                loginData: action.data,
+                isLoading: true,
             }
         }
 
-        case LOG_OUT: {
+        case LOG_IN_SUCCESS: {
+            return {
+                ...state,
+                isLoggedIn: true,
+                user: dummyUser,                
+                isLoading: false,
+            }
+        }
+
+        case LOG_OUT_REQUEST: {
             return {
                 ...state,
                 isLoggedIn: false,
@@ -56,7 +74,7 @@ const reducer = (state = initialState, action) => {
             }
         }
 
-        case SIGN_UP: {
+        case SIGN_UP_REQUEST: {
             return {
                 ...state,
                 signUpData: action.data
