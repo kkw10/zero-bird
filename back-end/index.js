0d094b4624e5ca02ad14c85393cfb0f7a2301757
kkw_10 <kkw_10@naver.com>
2019-09-11 16:12:46 +0900

    기능: 백엔드 서버 api 구조 설계

--- back-end/index.js
+++ back-end/index.js
@@ -3,15 +3,17 @@ const db = require('./models')
 const app = express();
 const PORT = 1991;
 
+const userAPIRouter = require('./routes/user');
+const postAPIRouter = require('./routes/post');
+const postsAPIRouter = require('./routes/posts');
+
 db.sequelize.sync();
 
-app.get('/', (req, res) => {
-    res.send('Hello, server')
-})
+app.use('/api/user', userAPIRouter)
 
-app.get('/about', (req, res) => {
-    res.send("I'm Koon")
-})
+app.use('/api/post', postAPIRouter)
+
+app.use('/api/posts', postsAPIRouter)
 
 app.listen(PORT, () => {
     console.log(`server is running... on localhost:${PORT}`)
