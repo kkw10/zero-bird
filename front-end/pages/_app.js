dfbdcb53b95a5589d771df21a6735aa60abc2557
kkw_10 <kkw_10@naver.com>
2019-09-25 00:06:21 +0900

    기능: react-slick을 이용해서 게시글 이미지 슬라이더 구현

--- front-end/pages/_app.js
+++ front-end/pages/_app.js
@@ -16,6 +16,8 @@ const ZeroBird = ({ Component, store, pageProps }) => {
             <Head>
                 <title>Zero bird</title>
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.22.2/antd.min.css"/>
+                <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
+                <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
             </Head>
             <AppLayout>
                 <Component {...pageProps} />                 
