99e2259fe71f1ee5dbc5037f4b8416b2d9717172
kkw_10 <kkw_10@naver.com>
2019-09-27 22:01:16 +0900

    기능: react helmet ssr적용 및 image zoom페이지 styled-components적용

--- front-end/pages/_app.js
+++ front-end/pages/_app.js
@@ -1,7 +1,6 @@
 // next.js에서 사용하는 layout 전용 컴포넌트
 import React from 'react';
 import PropTypes from 'prop-types';
-import Head from 'next/head';
 import AppLayout from '../components/AppLayout';
 import withRedux from 'next-redux-wrapper';
 import withReduxSaga from 'next-redux-saga'; // next용 redux saga
@@ -13,20 +12,36 @@ import rootSaga from '../sagas';
 import { LOAD_USER_REQUEST } from '../reducers/user';
 import axios from 'axios';
 import createSagaMiddleware from 'redux-saga';
+import Helmet from 'react-helmet';
+import { Container } from 'next/app';
 
 const ZeroBird = ({ Component, store, pageProps }) => {
     return (
-        <Provider store={store}>
-            <Head>
-                <title>Zero bird</title>
-                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.22.2/antd.min.css"/>
-                <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
-                <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
-            </Head>
-            <AppLayout>
-                <Component {...pageProps} />                 
-            </AppLayout>                    
-        </Provider>
+        <Container>
+            <Provider store={store}>
+                <Helmet 
+                    title="ZeroBird"
+                    htmlAttributes={{ lang: 'ko' }}
+                    meta={[
+                        {charset: 'UTF-8'},
+                        {name: 'viewport', content: "width=device-width, initial-scale=1.0"},
+                        {'http-equiv': 'X-UA-Compatible', content: 'IE=edge'},
+                        {name: 'description', content: 'Koon의 ZeroBird SNS'},
+                        {name: 'og:title', content: 'ZeroBird'},
+                        {name: 'og:description', content: 'Koon의 ZeroBird SNS'},
+                        {property: 'og:type', content: 'website'}
+                    ]}
+                    link={[
+                        {rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/antd/3.22.2/antd.min.css'},
+                        {rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css'},
+                        {rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css'}
+                    ]}
+                />
+                <AppLayout>
+                    <Component {...pageProps} />                 
+                </AppLayout>                    
+            </Provider>            
+        </Container>
     )
 }
 
--- front-end/pages/post.js
+++ front-end/pages/post.js
@@ -2,15 +2,29 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import { LOAD_POST_REQUEST } from '../reducers/post';
+import Helmet from 'react-helmet';
 
 const Post = ({ id }) => {
     const { singlePost } = useSelector(state => state.post);
 
     return (
         <>
+            <Helmet 
+                title={`${singlePost.User.nickname}님의 포스트`}
+                description={singlePost.content}
+                meta={[
+                    {name: 'description', content: singlePost.content},
+                    {property: 'og:title', content: `${singlePost.User.nickname}님의 포스트`},
+                    {property: 'og:description', content: singlePost.content},
+                    {property: 'og:image', content: singlePost.Images[0] && `http://localhost:1991/${singlePost.Images[0].src}` },
+                    {property: 'og:url', content: `http://localhost:1990/post/${id}`}
+                ]}
+            />
             <div>{singlePost.content}</div>
             <div>{singlePost.User.nickname}</div>
-            <div>{singlePost.Images[0] && <img src={`http://localhost:1991/${singlePost.Images[0].src}`} />}</div>
+            <div>
+                {singlePost.Images[0] && <img src={`http://localhost:1991/${singlePost.Images[0].src}`} />}
+            </div>
         </>
     )
 }
